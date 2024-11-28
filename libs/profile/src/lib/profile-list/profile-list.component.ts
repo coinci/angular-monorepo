2d861d8704473520f5c80da869e150b4c46755ac
coinci <dayan13@live.cn>
2024-11-28 16:57:27 +0800

    add base expander check

--- libs/profile/src/lib/profile-list/profile-list.component.ts
+++ libs/profile/src/lib/profile-list/profile-list.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MatIconModule } from '@angular/material/icon';
+import { ProfileComponent } from '../profile/profile.component';
 
 @Component({
   selector: 'lib-profile-list',
   standalone: true,
-  imports: [CommonModule, NgbModule, MatIconModule],
+  imports: [CommonModule, NgbModule, MatIconModule, ProfileComponent],
   templateUrl: './profile-list.component.html',
   styleUrl: './profile-list.component.css',
 })
--- libs/profile/src/lib/profile/profile.component.ts
+++ libs/profile/src/lib/profile/profile.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, viewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MatAccordion, MatExpansionModule } from '@angular/material/expansion';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'lib-profile',
   standalone: true,
-  imports: [CommonModule, NgbModule],
+  imports: [
+    CommonModule,
+    NgbModule,
+    MatExpansionModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css',
 })
-export class ProfileComponent {}
+export class ProfileComponent {
+  accordion = viewChild.required(MatAccordion);
+}
